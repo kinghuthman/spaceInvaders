37db115c24101c5028483a6d84e8852dd8117222
Huthman <kinghuthman@gmail.com>
2019-08-12 18:38:57 -0400

    fire missiles'

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 // hero
-const hero = {
+var hero = {
     top: 700,
-    left: 500
+    left: 550
 }
-
+// stores the position of the missile starting from where the hero is, using an object
+var missiles = []
 // move hero
 document.onkeydown = function(e) {
     if (e.keyCode === 37) {
@@ -11,11 +12,18 @@ document.onkeydown = function(e) {
         // want to move it from wherever it is
         hero.left = hero.left - 10;
         moveHero('left')
-    }
-    if (e.keyCode === 39) {
+    } else if (e.keyCode === 39) {
         console.log('right')
         hero.left = hero.left + 10;
         moveHero('right')
+    } else if (e.keyCode === 32) {
+        console.log('pew pew');
+        // first missile that is fired, position is added to array
+        missiles.push({
+            left: hero.left + 20,
+            top: hero.top - 15
+        })
+        drawMissiles();
     }
 }
 
@@ -26,6 +34,12 @@ const moveHero = (args) => {
         document.getElementById('hero').style.left = hero.left + 'px';
         console.log(args)
     }
-    
 }
-moveHero();
+const drawMissiles = () => {
+    document.getElementById('missiles').innerHTML = "";
+    // loops through array of fired missiles
+    for (var missile = 0; missile < missiles.length; missile = missile + 1){
+        /* for every missile, a missile element will be added to the missles element with the position based off of where the hero was */ 
+        document.getElementById('missiles').innerHTML += `<div class='missile' style='left:${missiles[missile].left}px; top:${missiles[missile].top}px;'></div>`;  
+    }
+}
